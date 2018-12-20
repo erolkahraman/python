d35dae2164185abde082c1d5d8ae8d8593add45d
Erol Kahraman <erol.kahraman@gmail.com>
2018-12-20 21:44:15 +0000

    güncelleme yapıldı

--- OS Betikleri/disk_bilgileri.py
+++ OS Betikleri/disk_bilgileri.py
@@ -2,25 +2,56 @@ import os
 
 disk = input("Disk bilgisi [\"/dev/sda\",\"/dev/sdb\"] veya mounted fs [\"/mnt/nfs\",\"/root\"]: ")
 
-print(disk)
-
 
 class disk_bilgisi():
-	def __init__(self,dizin):
+	def __init__(self, dizin):
 		self.dizin = dizin
-		
-	def bilgileri_getir(self):
+
+	def tum_bilgileri_getir(self):
 		self.stat = os.statvfs(self.dizin)
 		self.blok_sayisi = self.stat.f_blocks
-	
-	def bos_alan(self):
-		self.bilgileri_getir()
-		return self.blok_sayisi * 1024
+		self.blok_boyutu = self.stat.f_bsize
+		self.serbest_blok_sayisi = self.stat.f_bavail
+		self.toplam_disk_alani = (self.blok_sayisi * self.blok_boyutu) / 2 ** 30
+		self.serbest_disk_alani = (self.serbest_blok_sayisi * self.blok_boyutu) / 2 ** 30
+		self.kullanilan_disk_alani = (self.toplam_disk_alani - self.serbest_disk_alani)
+		print("""
+		Toplam disk alanı\t\t: {:.2f}G
+		Serbest disk alanı\t\t: {:.2f}G
+		Kullanılan disk alanı\t: {:.2f}G
+		""".format(self.toplam_disk_alani, self.serbest_disk_alani, self.kullanilan_disk_alani))
 
-try:
-	abc = disk_bilgisi(disk)
-except OSError as oe:
-	print(oe)
-	#print("Belirttiginiz disk veya mounted fs bulunamadi...")
+	def serbest_disk_alani(self):
+		print(self.serbest_disk_alani)
 
-print(abc.bos_alan())
+disk_durum = disk_bilgisi(disk)
+disk_durum.serbest_disk_alani()
+"""
+while True:
+
+	print("""
+
+#	1. Boş disk alanı
+#	2. Kullanılan disk alanı
+#	3. Toplam disk alanı
+#	4. Tüm disk bilgileri
+#
+#	Çıkış [q] :
+	""")
+	secim = input("Seçiminiz > ")
+
+	if secim == "q":
+		break
+	elif secim == "1":
+		print("Boş disk alanı: %sG" % disk_durum.serbest_disk_alani())
+		break
+
+
+
+"""
+
+#disk_durumu = disk_bilgisi(disk)
+
+#disk_durumu.tum_bilgileri_getir()
+
+# print(disk_durumu.bos_alan())
