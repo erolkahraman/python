8f966f57e5af2631a6f6837cb2effdd771aa670b
Erol Kahraman <erol.kahraman@gmail.com>
2022-06-09 17:30:07 +0300

    .

---
+++ NET Betikleri/menu.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+choise=$(whiptail --menu "" --notags --title "Net Komutları" 10 40 3 "1" "sh ver" "2" "sh users" 3>&2 2>&1 1>&3)
+
+case $choise in
+    "1") result=$(python show_ver.py);;
+    "2") pass
+esac
+
+whiptail --msgbox "$result" 30 60
